31d1e5f202d964eb4f316754a76af28731a7bfee
edoatley <edoatley@gmail.com>
2024-10-28 15:10:17 +0000

    basic bootstrapping

---
+++ src/test/java/org/edoatley/ai/rest/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package org.edoatley.ai.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    @Test
+    void generateGreeting(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string("Hello there!"));
+    }
+}
